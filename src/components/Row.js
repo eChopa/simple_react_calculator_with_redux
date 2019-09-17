ed1f6156e8017dfefe37463e856c0a0977daa006
Evgeniy Cheropkin <echeropkin@gmail.com>
2019-09-17 19:27:59 +0300

    html fixes

--- src/components/Row.js
+++ src/components/Row.js
@@ -11,34 +11,28 @@ const mapDispatchToProps = (dispatch) => {
 
 const RowLayout = ({row, idInArray, changeRow,deleteRow}) => {
     return (
-        <li className={`row mb-0 justify-content-center alert ${idInArray%2===0? 'alert-primary':null}`}>
-                <div className="col-3 col-sm-3 col-md-1 mb-2 mb-md-0 text-center">
+        <li className={`row mb-0 justify-content-around alert ${idInArray%2===0? 'alert-primary':null}`}>
                     <select
-                        className="form-control"
+                        className="form-control col-3 col-md-2 col-lg-1 mb-2 mb-md-0 text-center"
                         onChange={(event)=>{changeRow({id: row.id, type: "sign", value: event.target.value})}}
                         defaultValue={row.sign}>
                         <option value="+">+</option>
                         <option value="-">-</option>
                     </select>
-                </div>
-            <div className="col-9 col-sm-3 col-md-2 mb-2 mb-md-0 text-center">
-                <input className="form-control text-center" value={row.value}
+                <input className="col-9 col-md-4 col-lg-3 mb-2 mb-md-0 text-center form-control text-center"
+                       value={row.value}
                        onChange={(event)=>{changeRow({id: row.id, type: "value", value: event.target.value})}}
                        type="number"/>
-            </div>
-            <div className="col-6 col-sm-3 col-md-1 text-center">
                 <button
-                    className={`btn ${row.enable? 'btn-warning': 'btn-success'}`}
+                    className={`col-6 col-md-2 col-lg-1 text-center btn ${row.enable? 'btn-warning': 'btn-success'}`}
                     onClick={(event)=>{changeRow({id: row.id, type: "enable", value: !row.enable})}}>
-                    {row.enable? 'Disable' : 'Enable'}</button>
-            </div>
-            <div className="col-6 col-sm-3 col-md-1 text-center">
+                    {row.enable? 'Disable' : 'Enable'}
+                 </button>
                 <button
-                    className="btn btn-danger"
-                onClick={()=>{deleteRow(row.id)}}>
+                    className="col-6 col-md-2 col-lg-1 text-center btn btn-danger"
+                    onClick={()=>{deleteRow(row.id)}}>
                     Delete
                 </button>
-            </div>
         </li>
     )
 }
